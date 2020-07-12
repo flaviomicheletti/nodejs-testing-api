1397ca128fd3c1035b175b561332b380062872b9
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-11 23:59:17 -0300

    code

--- src/app.js
+++ src/app.js
@@ -6,4 +6,20 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
 });
 
+app.get("/random", (req, res) => {
+    const rand = Math.floor(Math.random() * 10);
+    return res.status(200).json({ value: rand });
+});
+
+
+app.get("/foo", (req, res) => {
+    const rand = Math.floor(Math.random() * 2);
+
+    if (rand) {
+        res.status(200).json({value: rand});
+    } else  {
+        res.status(400).json({value: rand});
+    }
+});
+
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 //server.js
 const app = require("./app");
 
-app.listen(5678, () => {
-    console.log("Example app listening on port 5678!");
+app.listen(5000, () => {
+    console.log("Example app listening on port 5000!");
 });
