a9a5af35504d751c8687e429fba8642c907c42b3
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-11 23:59:22 -0300

    test

--- test/supertest-way.test.js
+++ test/supertest-way.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const request = require("supertest");
 const app = require("../src/app");
 
@@ -6,5 +7,18 @@ describe("Test the root path", () => {
     return request(app)
       .get("/")
       .expect(200);
+  });
+
+  describe('GET /random', function () {
+    it('responds with json', function () {
+      return request(app)
+        .get('/random')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          assert(true, Number.isInteger(response.body.value))
+        })
+    });
   });
 });
